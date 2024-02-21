08ca018b83359d4bf422a8e1a98217e1e3e0a294
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-21 13:00:44 +0600

    footer and error page added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
      return (
           <nav>
-              <Link to=''>Home</Link>
-              <Link to=''>Course</Link>
-              <Link to=''>Pricing</Link>
-              <Link to=''>Contact</Link>
+              <Link to='/'>Home</Link>
+              <Link to='courses'>Courses</Link>
+              <Link to='pricing'>Pricing</Link>
+              <Link to='contact'>Contact</Link>
           </nav>
      );
 };
