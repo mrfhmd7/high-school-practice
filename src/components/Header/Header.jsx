1563463ae8a14612beabcf24e64635ddf7032e62
mrfhmd7 <mrf.hmd7@gmail.com>
2024-03-05 21:07:02 +0600

    home section complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,6 @@ const Header = () => {
                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ml-1">
                               <path fillRule="evenodd" d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z" clipRule="evenodd" />
                          </svg>
-
                     </button>
                </div>
           </nav>
