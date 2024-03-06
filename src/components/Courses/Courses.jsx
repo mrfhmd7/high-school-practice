7b4befeb1e00c5b057b6d4812c40b981f4e365df
mrfhmd7 <mrf.hmd7@gmail.com>
2024-03-06 23:51:20 +0600

    get all info in courses section

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const Courses = () => {
      return (
           <div>
-               <h2>All courses here.</h2>
+               <div>
+                    <h2>Affordable Packages</h2>
+                    <p>Problems trying to resolve the conflict between the two major realms of Classical physics:
+                         <br />
+                         Newtonian mechanics
+                    </p>
+                    <button className='text-[#96BB7C] border-[#96BB7C] rounded-md border-2 pr-4 pl-4 font-bold'>Learn More</button>
+               </div>
+               <div>
+                    <div>
+                         <img src="blackboards.png" alt="" />
+                         <h4>Certified Teacher</h4>
+                         <p>The gradual accumulation of information about </p>
+                    </div>
+                    <div>
+                         <img src="telescope-1.png" alt="" />
+                         <h4>Expert instruction</h4>
+                         <p>The gradual accumulation of information about</p>
+                    </div>
+               </div>
           </div>
      );
 };
