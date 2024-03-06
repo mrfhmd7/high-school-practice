14a54694989e9cb0d2819c8aa8308b6842a59a08
mrfhmd7 <mrf.hmd7@gmail.com>
2024-03-07 00:13:02 +0600

    another section added

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,28 +2,41 @@ import React from 'react';
 
 const Courses = () => {
      return (
-          <div className='flex justify-between text-start mt-16'>
-               <div>
-                    <h className="text-4xl font-bold">Affordable Packages</h>
-                    <p className='font-semibold mt-10 text-[#737373]'>Problems trying to resolve the conflict between the two major realms of Classical physics:
-                         <br />
-                         Newtonian mechanics
-                    </p>
-                    <button className='text-[#96BB7C] border-[#96BB7C] mt-10 rounded-md border-2 pr-4 pt-3 pb-3 pl-4 font-bold'>Learn More</button>
-               </div>
-               <div className='flex justify-between gap-4'>
-                    <div className='shadow-md rounded-md pl-6 pt-6'>
-                         <img className='bg-[#96BB7C] p-2 rounded' src="blackboards.png" alt="" />
-                         <h4 className='text-lg font-bold mt-4'>Certified Teacher</h4>
-                         <p className='font-semibold text-[#737373] mt-4'>The gradual accumulation of information about </p>
+          <>
+               <div className='flex justify-between text-start mt-16'>
+                    <div>
+                         <h className="text-4xl font-bold">Affordable Packages</h>
+                         <p className='font-semibold mt-10 text-[#737373]'>Problems trying to resolve the conflict between the two major realms of Classical physics:
+                              <br />
+                              Newtonian mechanics
+                         </p>
+                         <button className='text-[#96BB7C] border-[#96BB7C] mt-10 rounded-md border-2 pr-4 pt-3 pb-3 pl-4 font-bold'>Learn More</button>
                     </div>
-                    <div className='shadow-md rounded-md pl-6 pt-6'>
-                         <img className='bg-[#96BB7C] p-2 rounded' src="telescope-1.png" alt="" />
-                         <h4 className='text-lg font-bold mt-4'>Expert instruction</h4>
-                         <p className='font-semibold text-[#737373] mt-4'>The gradual accumulation of information about</p>
+                    <div className='flex justify-between gap-4'>
+                         <div className='shadow-md rounded-md pl-6 pt-6'>
+                              <img className='bg-[#96BB7C] p-2 rounded' src="blackboards.png" alt="" />
+                              <h4 className='text-lg font-bold mt-4'>Certified Teacher</h4>
+                              <p className='font-semibold text-[#737373] mt-4'>The gradual accumulation of information about </p>
+                         </div>
+                         <div className='shadow-md rounded-md pl-6 pt-6'>
+                              <img className='bg-[#96BB7C] p-2 rounded' src="telescope-1.png" alt="" />
+                              <h4 className='text-lg font-bold mt-4'>Expert instruction</h4>
+                              <p className='font-semibold text-[#737373] mt-4'>The gradual accumulation of information about</p>
+                         </div>
                     </div>
                </div>
-          </div>
+               <div className='flex text-start mt-20 items-center'>
+                    <img className='w-1/2' src="c-lady-1.png" alt="" />
+                    <div>
+                         <h2 className='font-bold text-5xl'>Video in Live Action</h2>
+                         <p className='text-[#737373] text-lg font-semibold mt-5'>Problems trying to resolve the conflict between the two major realms of Classical physics:
+                              <br />
+                              Newtonian mechanics
+                         </p>
+                         <button className='text-[#96BB7C] border-[#96BB7C] mt-10 rounded-md border-2 pr-4 pt-3 pb-3 pl-4 font-bold'>Learn More</button>
+                    </div>
+               </div>
+          </>
      );
 };
 
